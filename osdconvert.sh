1ce0f4147b30e20f1007000a26208fdaa711bf11
Dan Krause <dan@dankrause.net>
2012-12-21 13:21:09 -0500

    now ramping up OSD weight during rebalancing

--- osdconvert.sh
+++ osdconvert.sh
@@ -8,19 +8,28 @@ function die { echo "Error: $1" 1>&2 ; usage ; }
 function usage { echo "Usage: $0 [-o OSDNUM|-a]" ; exit 1 ; }
 
 function rebalance {
-	echo -n "Waiting for a healthy cluster"
-	while true ; do
-		OSDCOUNT=$1
-		read OSDUP OSDIN <<< $(ceph osd stat | awk '{print $4 " " $6}')
-		HEALTH=$(ceph health)
-		if [ "$OSDCOUNT" -eq "$OSDUP" -a "$OSDCOUNT" -eq "$OSDIN" -a "$HEALTH" == "HEALTH_OK" ] ; then
-			echo " $HEALTH"
-			break
-		else
-			echo -n "."
-		fi
-		sleep 10
+	for HEALTH_CHECK in "HEALTH_WARN" "HEALTH_OK" ; do
+	    echo -n "Waiting for $HEALTH_CHECK"
+	    while true ; do
+		    OSDCOUNT=$1
+		    read OSDUP OSDIN <<< $(ceph osd stat | awk '{print $4 " " $6}')
+		    HEALTH=$(ceph health | awk '{print $1}')
+		    if [ "$OSDCOUNT" -eq "$OSDUP" -a "$OSDCOUNT" -eq "$OSDIN" -a "$HEALTH" == "$HEALTH_CHECK" ] ; then
+			    echo " $HEALTH"
+			    break
+		    else
+			    echo -n "."
+		    fi
+		    sleep 10
+	    done
 	done
+}
+
+function weightramp {
+    for WEIGHT in $(seq -w "$1" "$1" 1) ; do
+        ceph osd reweight "$2" "$WEIGHT"
+        rebalance "$TOTALOSDCOUNT"
+    done
 }
 
 OSDLIST=""
@@ -40,6 +49,15 @@ while getopts "o:a" OPTION ; do
 done
 test -n "$OSDLIST" || die "please use -o \$OSD or -a"
 
+TOTALOSDCOUNT=$(ceph osd stat | awk '{print $2}')
+
+HEALTH=$(ceph health | awk '{print $1}')
+
+if [ "$HEALTH" -ne "HEALTH_OK" ]; then
+    echo "Ceph cluster health is not HEALTH_OK - refusing to start"
+    exit 1
+fi
+
 for OSD in $OSDLIST ; do
 	DEVPATH=$(ceph-conf -n osd.$OSD "btrfs devs")
 	DATAPATH=$(ceph-conf -n osd.$OSD "osd data")
@@ -47,15 +65,13 @@ for OSD in $OSDLIST ; do
 
 	test $(ceph-conf -n osd.$OSD "host") == $(hostname -s) || die "osd.$OSD does not live on this host"
 
-	rebalance "$(ceph osd stat | awk '{print $2}')"
-
 	echo "Rebuilding osd $OSD with XFS on $DEVPATH"
 	echo "--------------------------------------"
 	service ceph stop osd.$OSD
 	ceph osd down $OSD
 	ceph osd out $OSD
 
-	rebalance "$(ceph osd stat | awk '{print $2-1}')"
+	rebalance $((TOTALOSDCOUNT-1))
 
 	ceph osd rm $OSD
 
@@ -70,5 +86,11 @@ for OSD in $OSDLIST ; do
 	ceph-osd -i $OSD --mkfs --mkkey --mkjournal
 	ceph auth add osd.$OSD osd 'allow *' mon 'allow rwx' -i $DATAPATH/keyring
 	grep "$DEVPATH" /etc/fstab || echo -e "$DEVPATH\t$DATAPATH\txfs\tdefaults\t0\t2" >> /etc/fstab
+	
+	ceph osd reweight $OSD 0
+	
 	service ceph start osd.$OSD
+
+	weightramp "0.01" $OSD
+
 done
