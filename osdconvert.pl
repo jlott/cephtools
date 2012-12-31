55ee7afd9a17d8552567807a3b48dec6eac68308
Justin Lott <jlott@liquidweb.com>
2012-12-31 14:04:03 -0500

    More robust health checking

--- osdconvert.pl
+++ osdconvert.pl
@@ -8,12 +8,6 @@ use IPC::System::Simple qw(capture);
 
 $| = 1;
 
-chomp (my $health = capture('ceph health'));
-if ($health ne "HEALTH_OK") {
-	print "Refusing to start work on an unhealthy cluster.\n";
-	exit;
-}
-
 my @osds;
 for (capture("ceph-conf -l osd. --filter-key-value host=\$(hostname)")) {
 	chomp;
@@ -24,6 +18,7 @@ foreach (@osds) {
 	if (`mount -t xfs | grep $_`) {
 		&timestamp; print "$_ appears to be mounted as XFS. Skipping.\n";
 	} else {
+		&wait_health_good(0);
 		&timestamp(); print "Starting conversion of $_\n";
 		my ($num) = ($_ =~ m/osd\.(\d+)/);
 		chomp (my $path = capture("ceph-conf -n $_ \"btrfs devs\""));
@@ -33,7 +28,7 @@ foreach (@osds) {
 		&timestamp; print "Marking $_ down... "; capture("ceph osd down $num"); print "done.\n";
 		&timestamp; print "Marking $_ out... "; capture("ceph osd out $num"); print "done.\n";
 		&wait_health_bad();
-		&wait_health_good();
+		&wait_health_good(1);
 		&timestamp; print "Removing $_ from the crush map... "; capture("ceph osd rm $num"); print "done.\n";
 		# FIXME this should really time out and die eventually
 		&timestamp; print "Waiting for file handles on $mountpoint to be released... "; do { sleep 1; } while (`lsof | grep $mountpoint`); print "done.\n";
@@ -49,7 +44,7 @@ foreach (@osds) {
 		for (my $weight = 0.01; $weight <= 1; $weight += 0.01) {
 			&timestamp; print "Setting weight of $_ to $weight... "; capture("ceph osd reweight $num $weight"); print "done.\n";
 			&wait_health_bad();
-			&wait_health_good();
+			&wait_health_good(0);
 			&timestamp; print "Sleeping for 300 seconds... "; sleep 300; print "done.\n";
 		}
 		&timestamp; print "Conversion of $_ complete!\n\n";
@@ -88,10 +83,13 @@ sub wait_health_bad {
 
 # Wait indefinitely for a healthy cluster
 sub wait_health_good {
+	my $allowed_down = $_[0];
 	&timestamp; print "Waiting for healthy cluster... ";
 	while () {
+		my ($osd_tot, $osd_up, $osd_in) = (`ceph osd stat` =~ /e\d+: (\d+) osds: (\d+) up, (\d+) in/);
+		$osd_tot -= $allowed_down;
 		chomp (my $health = capture('ceph health'));
-		if ($health ne "HEALTH_OK") {
+		if ($health ne "HEALTH_OK" || $osd_tot ne $osd_up || $osd_tot ne $osd_in) {
 			sleep 1;
 		} else {
 			print "done.\n";
